c5ad28f2a246f4fa898953be74a01a52141d4584
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-20 16:49:03 +0100

    auth setup

--- public/script.js
+++ public/script.js
@@ -1,7 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +13,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const gBut = () => {
+    alert("jkwk")
+}
+window.gBut = gBut
