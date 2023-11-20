350c909b68ef22572e4ff95382069a6c1ce33650
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-20 16:34:49 +0100

    firebase added

---
+++ public/script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMqc7bv0ZOHam60zdaoZXMtHfNWXBwCRo",
+  authDomain: "full-project-81848.firebaseapp.com",
+  projectId: "full-project-81848",
+  storageBucket: "full-project-81848.appspot.com",
+  messagingSenderId: "950678778632",
+  appId: "1:950678778632:web:ed95cea3d3f8eacc1132bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
