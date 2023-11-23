2dddfba9342c81b95c78956474c1d82f786f306a
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-11-23 14:31:33 +0100

    testing env 2

---
+++ public/config.js
@@ -0,0 +1,9 @@
+const firebaseConfigs = {
+    apiKey: "AIzaSyBMqc7bv0ZOHam60zdaoZXMtHfNWXBwCRo",
+    authDomain: "full-project-81848.firebaseapp.com",
+    projectId: "full-project-81848",
+    storageBucket: "full-project-81848.appspot.com",
+    messagingSenderId: "950678778632",
+    appId: "1:950678778632:web:ed95cea3d3f8eacc1132bd",
+    databaseURL: "https://full-project-81848-default-rtdb.firebaseio.com"
+};
